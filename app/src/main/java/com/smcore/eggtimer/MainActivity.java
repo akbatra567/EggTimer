d0d73340ea429502c672cd9a70a3947f41f3667c
Akshit <hab2997@gmail.com>
2020-01-07 01:16:03 +0530

    Working Egg Timer code android

---
+++ app/src/main/java/com/smcore/eggtimer/MainActivity.java
@@ -0,0 +1,103 @@
+package com.smcore.eggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView timerTextView;
+    private SeekBar timer;
+    boolean counterIsActive = false;
+    CountDownTimer countdown;
+    public void updateTimer(int secondsLeft){
+
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9){
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // find views
+        timer = findViewById(R.id.set_timer);
+        ImageView egg = findViewById(R.id.egg);
+        timerTextView = findViewById(R.id.timer);
+        final Button go = findViewById(R.id.start);
+
+        timer.setMax(600);
+        timer.setProgress(30);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( !counterIsActive ) {
+                    counterIsActive = true;
+                    timer.setEnabled(false);
+
+                    // plus 100 miliseconds to process the first tick and put timer on time;
+                        countdown = new CountDownTimer(timer.getProgress() * 1000 + 100, 1000) {
+                        @Override
+                        public void onTick(long l) {
+                            updateTimer((int) l / 1000);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            timerTextView.setText("0:00");
+                            MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sound);
+                            mediaPlayer.start();
+                        }
+                    }.start();
+                    go.setText("Stop");
+                }
+                else{
+                    timerTextView.setText("0:30");
+                    timer.setProgress(30);
+                    countdown.cancel();
+                    timer.setEnabled(true);
+                    go.setText("GO!");
+                    counterIsActive = false;
+                }
+            }
+        });
+        timer.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean user) {
+                updateTimer(progress);
+                Log.i("verbose", timerTextView.getText().toString());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
